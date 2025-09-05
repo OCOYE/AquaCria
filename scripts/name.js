352085033e48e81b83c716682b4ae85b5109d3a1
MANOEL ANTÔNIO <manoelantonio3412@gmail.com>
2025-09-05 19:53:56 -0300

    a
    a

--- scripts/name.js
+++ scripts/name.js
@@ -3,7 +3,7 @@ let name = window.prompt("Qual é o seu nome?");
 // Verifica se o usuário digitou "gamer" (case-insensitive)
 if (name.toLowerCase() === "gamer") {
     // Redireciona para gamer.html
-    window.location.href = "/eggs/gamer.html";
+    window.location.href = "/eggs/gamer.html/";
 } else {
     // Opcional: mensagem caso não seja "gamer"
     alert("Olá, " + name + "!");
