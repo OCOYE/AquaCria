b9c21a5ca53bf49a93c080f26146608586e15a24
MANOEL ANTÔNIO <manoelantonio3412@gmail.com>
2025-09-05 20:01:04 -0300

    ss
    ss

--- scripts/name.js
+++ scripts/name.js
@@ -3,7 +3,7 @@ let name = window.prompt("Qual é o seu nome?");
 // Verifica se o nome digitado é "gamer" (case-insensitive)
 if (name && name.toLowerCase() === "gamer") {
     // Redireciona para outra página do seu site
-    window.location.href = "https://ocoye.github.io/AquaCria/eggs/gamer.html"; // substitua pelo link real
+    window.location.href = "./eggs/gamer.html"; // substitua pelo link real
 } else {
     alert("Bem-vindo, " + name + "!");
 }
