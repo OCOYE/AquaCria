03d264a9ea7cfedcd15f6d341016277d7665b8ec
MANOEL ANTÔNIO <manoelantonio3412@gmail.com>
2025-09-06 10:01:36 -0300

    sem easter eggs
    espero que ngm leia o name.js

--- scripts/name.js
+++ scripts/name.js
@@ -4,6 +4,26 @@ let name = window.prompt("Qual é o seu nome?");
 if (name && name.toLowerCase() === "gamer") {
     // Redireciona para outra página do seu site
     window.location.href = "./eggs/gamer.html"; // substitua pelo link real
+} else if (name && name.toLowerCase() === "aula05") {
+    // Redireciona para a página de admin
+    window.location.href = "https://www.youtube.com/watch?v=Eqypnw6gQNg";
+
+} else if (name && name.toLowerCase() === "173") {
+    // Redireciona para a página de admin
+    window.location.href = "https://www.youtube.com/watch?v=CGYuUc9pPtQ";
+
+} else if (name && name.toLowerCase() === "linux") {
+    // Redireciona para a página de admin
+    window.location.href = "https://www.youtube.com/watch?v=iYWzMvlj2RQ";
+
+} else if (name && name.toLowerCase() === "i_use_arch_btw") {
+    // Redireciona para a página de admin
+    window.location.href = "https://archlinux.org/";
+
+} else if (name && name.toLowerCase() === "a_and_b") {
+    // Redireciona para a página de admin
+    window.location.href = "https://www.youtube.com/watch?v=5BaH5bahjMA";
+
 } else {
-    alert("Bem-vindo, " + name + "!");
+    document.writeln(`${name}`)
 }
