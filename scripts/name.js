85640797913c2370cf047e6d22f49091c33c32ea
MANOEL ANTÔNIO <manoelantonio3412@gmail.com>
2025-09-06 12:27:44 -0300

    legal
    legal

--- scripts/name.js
+++ scripts/name.js
@@ -1,29 +1,18 @@
-let name = window.prompt("Qual é o seu nome?");
+let name = window.prompt("Qual é o seu nome?"); /* eu sei que voce esta lendo isso... */
 
-// Verifica se o nome digitado é "gamer" (case-insensitive)
+
 if (name && name.toLowerCase() === "gamer") {
-    // Redireciona para outra página do seu site
-    window.location.href = "./eggs/gamer.html"; // substitua pelo link real
+    window.location.href = "./eggs/gamer.html"; /* daqui para baixo tudo easter egg doido! */
 } else if (name && name.toLowerCase() === "aula05") {
-    // Redireciona para a página de admin
     window.location.href = "https://www.youtube.com/watch?v=Eqypnw6gQNg";
-
 } else if (name && name.toLowerCase() === "173") {
-    // Redireciona para a página de admin
     window.location.href = "https://www.youtube.com/watch?v=CGYuUc9pPtQ";
-
 } else if (name && name.toLowerCase() === "linux") {
-    // Redireciona para a página de admin
-    window.location.href = "https://www.youtube.com/watch?v=iYWzMvlj2RQ";
-
+    window.location.href = "kernel.org";
 } else if (name && name.toLowerCase() === "i_use_arch_btw") {
-    // Redireciona para a página de admin
     window.location.href = "https://archlinux.org/";
-
 } else if (name && name.toLowerCase() === "a_and_b") {
-    // Redireciona para a página de admin
     window.location.href = "https://www.youtube.com/watch?v=5BaH5bahjMA";
-
 } else {
-    document.writeln(`${name}`)
+    document.writeln(`${name}`) /* isso não é */
 }
